dd7aac4963a6416ebd3eee915dc18ab73de0e6c3
Puran Bahadur <144983194+Puran167@users.noreply.github.com>
2025-07-15 23:18:22 +0530

    Update sendEmail.js

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 const sendWelcomeEmail = async (to, username) => {
   try {
     await resend.emails.send({
-      from: 'onboarding@resend.dev',
+      from: 'noreply@resend.dev',
       to,
       subject: `Welcome to Messageflow 🎉`,
       html: `
